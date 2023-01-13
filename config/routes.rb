10d3a745906c7511b8b502301e08ebb4ec97f90f
Reiyirak <pdsmej@gmail.com>
2023-01-13 16:02:31 -0600

    Finished HTML for Admin CRUD
    We made the html that will serve as a base for the CRUD of our program
    admin
    
    Co-authored-by: MarcoRodriguez23 <rodriguezlmarco23@gmail.com>
    Co-authored-by: Yaxkin121 <ycardenas13@ucol.mx>
    Co-authored-by: MaxRetana <maxretana981@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'admin#index'
+  get '/home', to: 'home#index'
+  get '/options', to: 'options#index'
 end
