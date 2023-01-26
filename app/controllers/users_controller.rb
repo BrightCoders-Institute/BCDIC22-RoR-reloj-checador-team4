077ee53a37fc3948347dbcbc31e9fbf2ef7e2087
MaxRetana <maxretana981@gmail.com>
2023-01-25 18:25:17 -0600

    Create CRUD of employees
    We create the CRUD of the employee
    
    Co-authored-by: MarcoRodriguez23 <rodriguezlmarco23@gmail.com>
    Co-authored-by: Yaxkin121 <ycardenas13@ucol.mx>
    Co-authored-by: Reiyirak <pdsmej@gmail.com>

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.order(:id)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+  
+  def sleep
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      @user.update(status: 0)
+      redirect_to users_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:name, :email, :position, :private_num, :emp_num, :status, :company_id)
+  end
+end
