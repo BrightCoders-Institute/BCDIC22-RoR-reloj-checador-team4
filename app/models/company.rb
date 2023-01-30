13e100657a43d9120df6a15a2cba508664da5a86
MaxRetana <maxretana981@gmail.com>
2023-01-30 16:57:22 -0600

    Add validations
    We add some validations in the model of user and company and we chane the
    time zone, because in the checks's table had different hour.
    
    Co-authored-by: MarcoRodriguez23 <rodriguezlmarco23@gmail.com>
    Co-authored-by: Yaxkin121 <ycardenas13@ucol.mx>
    Co-authored-by: Reiyirak <pdsmej@gmail.com>

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ApplicationRecord
   has_many :users
-  validates :name, presence: true, length: {minimum: 1}
+  validates :name, presence: true, length: {minimum: 1}, uniqueness: true
   validates :address, presence: true, length: {minimum: 1}
 end
