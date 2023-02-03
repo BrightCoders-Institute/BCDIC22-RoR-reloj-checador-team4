657963d51eae3e3b8b0b9a9a267dd6ebb3278112
MaxRetana <maxretana981@gmail.com>
2023-02-02 18:44:20 -0600

    Fix indentation and delete lines
    We fix the indentation in the files of reports (absence, daily and monthly) and
    delete the lines of the time zone and the default time zone.
    
    Co-authored-by: MarcoRodriguez23 <rodriguezlmarco23@gmail.com>
    Co-authored-by: Yaxkin121 <ycardenas13@ucol.mx>
    Co-authored-by: Reiyirak <pdsmej@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,6 @@ module ClockChecker
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    #config.time_zone = "Guadalajara"
-    #config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
