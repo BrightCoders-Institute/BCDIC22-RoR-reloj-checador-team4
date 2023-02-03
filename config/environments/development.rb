d7ae56f3b58e1fb3764b912b662fc06814175e62
MaxRetana <maxretana981@gmail.com>
2023-02-03 16:25:23 -0600

    Add information in database file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  config.hosts << "bcdic22-ror-reloj-checador-team4-production.up.railway.app"
   # Do not eager load code on boot.
   config.eager_load = false
 
