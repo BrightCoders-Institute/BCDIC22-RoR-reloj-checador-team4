c8ec09c12d60fa267d4185c94dbf4cc1e6b5ba7f
MaxRetana <maxretana981@gmail.com>
2023-02-03 16:12:18 -0600

    add-information

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.require_master_key = true
+
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
